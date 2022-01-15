cd2ff0c9c3a81e28b49754b3d63b0784c35c4611
jkierucz <01169183@pw.edu.pl>
2022-01-15 20:15:00 +0100

    add contents and count attributes to Board

--- classes.py
+++ classes.py
@@ -1,9 +1,16 @@
-from exceptions import SizeError
+from exceptions import SizeError, CannotSolveError
+from CONST import sides
 
 
 class Board():
     def __init__(self, size: int = 0) -> None:
         self._size = self._check_size(size)
+        self.contents = [
+            [set(range(1, self._size+1)) for _ in range(self.size())]
+            for _ in range(self.size())]
+        # representation of the board: sets of possible values
+
+        self.count = {i: 0 for i in range(1, self.size()+1)}
 
     @staticmethod
     def _check_size(size):
