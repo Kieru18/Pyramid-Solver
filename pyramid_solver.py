578a3ef54a0b8af0dbfc82d9dac6a46253a131d5
jkierucz <01169183@pw.edu.pl>
2022-01-19 13:00:16 +0100

    incorporated simple argparser interface, solution is currently printed

--- pyramid_solver.py
+++ pyramid_solver.py
@@ -1,22 +1,23 @@
 import sys
-import argparse
 from class_board import Board
 from class_clues import Clues
+import argparse
 
 
 def main(argv):
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('-i', "--input",
-    # help="accept input from command line",
-    #                     action="store_true")
-    # args = parser.parse_args()
-    # if args.input:
-    #     print("a")
-    input = Clues("data.txt")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", action='store',
+                        help="path to input file")
+    args = parser.parse_args(argv[1:])
+    input = Clues(args.input)
     size = len(input.clues[0])
     # @TODO - checking input for size corectness and cotradictions
-    board = Board(size)
+    board = Board(size, clues=input.clues)
     board.solve_initial_clues()
+    board.prep_subsidiary_board()
+    board.solve_board()
+    for row in board.board:
+        print(row)
 
 
 if __name__ == "__main__":
